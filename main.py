5cbb0133d17d1f12a587b67dbd0de58ef2b990d1
cyberneel <neelesh.chevuri25@gmail.com>
2024-02-05 16:09:08 -0600

    Initial New Rework
    Added new custom trained model. Going to start reworking features.

---
+++ main.py
@@ -0,0 +1,36 @@
+# Author: Neelesh Chevuri
+
+import torch
+import torch.nn as nn
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+import streamlit as st
+from modules import get_classes_len, visualize_model_predictions
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model_ft = models.resnet18(weights='IMAGENET1K_V1')
+num_ftrs = model_ft.fc.in_features
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
+model_ft.fc = nn.Linear(num_ftrs, get_classes_len())
+
+model_ft = model_ft.to(device)
+model_ft.load_state_dict(torch.load('dermalyze-ai-resnet-ft-4_exp.pth'))
+model_ft.eval()
+
+uploaded_img = st.file_uploader("Upload an Image! (jpg)")
+
+if uploaded_img is not None:
+    visualize_model_predictions(model_ft, img_path=uploaded_img)
+
+#visualize_model_predictions(
+ #   model_ft,
+  #  img_path='S_0917_acne_M1080444.2e16d0ba.fill-920x613.jpg'
+#)
+
---
+++ modules.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+import streamlit as st
+
+# Transforms for input images
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ]),
+}
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class_names = ['acne', 'eczema', 'melanoma & moles', 'psoriasis']
+
+def get_classes_len() -> int:
+    return len(class_names)
+
+def visualize_model_predictions(model,img_path):
+    was_training = model.training
+    model.eval()
+
+    img = Image.open(img_path)
+    img = data_transforms['val'](img)
+    img = img.unsqueeze(0)
+    img = img.to(device)
+
+    with torch.no_grad():
+        outputs = model(img)
+        _, preds = torch.max(outputs, 1)
+        
+        st.write(class_names[preds[0]])
+
+        model.train(mode=was_training)
